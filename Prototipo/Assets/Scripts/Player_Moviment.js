32cd0f2a87e3c4b1c7eec75225523fa9b66ca5ad
Rannyel Lino <rannyellino@gmail.com>
2017-08-23 21:30:47 -0300

    1.0.1
    Alteração no script da movimentação do player e adição do script responsavel pela animação da câmera

--- Prototipo/Assets/Scripts/Player_Moviment.js
+++ Prototipo/Assets/Scripts/Player_Moviment.js
@@ -2,6 +2,10 @@
 
 var speed = 4.5f; //Variavel que corresponde a velocidade de movimento do jogador
 var where: int; //Serve para indentificar onde o jogador está no tabuleiro
+var canMove: boolean; //Variavel que só tem utilidade para o movimento para baixo, verifica quando pode se mover(true) e após a 
+                      //movimentação ele passa a receber false.
+static var cameraMove: boolean; //Funciona para a camera saber quando pode se movimentar para acompanhar o player
+                                //Esa variavel é static por que ela é global para o script "Scene_Camera" poder usa-lo 
 
 function Start () {
 	where = 0; // 0 = Centro / 1 = Topo / -1 = Baixo
@@ -11,36 +15,41 @@ function Update () {
 	Moviment();
 }
 
-// Função responsavel pela movimentação do jogador
-function Moviment () {
+// Função responsavel pela movimentação do jogador, não é usado o método "Translate" por não dependermos de uma movimentação
+//fluida e sim de uma movimentação estática.
+function Moviment () { 
 	if(Input.GetKeyDown(KeyCode.UpArrow))
 	{
 		if(where == 0)
 		{
-			transform.position.y += 1;
+			transform.position.y += 1.6; //Esse valor é fixo para por causa das divisões do tabuleiro.
 			where = 1;
 		}
 		if(where == -1)
 		{
-			transform.position.y += 1;
+			transform.position.y += 1.6; //Esse valor é fixo para por causa das divisões do tabuleiro.
 			where = 0;
 		}
 	}
-	if(Input.GetKey(KeyCode.DownArrow))
+	if(Input.GetKeyDown(KeyCode.DownArrow))
 	{
-		if(where == 1)
+	canMove = true;
+		if(where == 1 && canMove == true)
 		{
-			transform.position.y -= 1;
+			transform.position.y -= 1.6; //Esse valor é fixo para por causa das divisões do tabuleiro.
 			where = 0;
+			canMove = false;
 		}
-		else if(where == 0)
+		if(where == 0 && canMove == true)
 		{
-			transform.position.y -= 1;
+			transform.position.y -= 1.6; //Esse valor é fixo para por causa das divisões do tabuleiro.
 			where = -1;
+			canMove = false;
 		}
 	}
-	if(Input.GetKey(KeyCode.RightArrow))
+	if(Input.GetKeyDown(KeyCode.RightArrow))
 	{
-		transform.position.x += 1;
+		transform.position.x += 1.7; //Esse valor é fixo para por causa das divisões do tabuleiro.
+		cameraMove = true; //Colocamos a variavel true somente nessa ocasião mas só nos importa quando o jogador anda para frente.
 	}
 }
---
+++ Prototipo/Assets/Scripts/Scene_Camera.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+var speedCam = 9.0; //Velocidade a movimentação da camera
+var oldX: float; //Saber até onde a camera pode ir
+var animationCamera: boolean; //Serve para saber quando se pode começar a movimentar a camera
+var firstTime: boolean; //A primeira movimentação é diferente das outras
+var whereChangeSum: int; //Serve para sabermos e limitarmos as movimentações da camera, pois de 4 em 4 movimentações alteramos o valor da soma
+function Start () {
+	oldX = 1.66; //O primeiro valor do oldX é estático 
+}
+
+function Update () {
+	Moviment();
+}
+
+function Moviment() {
+	//Está primeira condição funciona para sabermos quando o jogador se movimentou
+	if(Player_Moviment.cameraMove == true)
+	{	
+		animationCamera = true;	
+		Player_Moviment.cameraMove = false;
+		if(firstTime == true && whereChangeSum < 3)
+		{
+			oldX = oldX+1.7; //A partir da segunda movimentação a limitação de até onde a camera pode se movimentar muda, no começo
+			                 //somamos 1.66 para a movimentação, após isso soma 1.7 até chegar na próxima quinta movimentação que ai
+			                 //sim somamos 1.66 novamente.
+		}
+		else
+		{
+			if(firstTime == true)
+			{
+				oldX = oldX+1.66;
+				whereChangeSum = 0;
+			}
+		}
+	}
+
+	//Quando o script descobre que já se pode movimentar usamos o "Translate" com a váriavel speedCam
+	if(animationCamera == true)
+	{
+		transform.Translate(speedCam*Time.deltaTime, 0, 0);
+	if(transform.position.x > oldX) //Aqui limitamos até onde a câmera pode chegar.
+	{
+		animationCamera = false;
+		firstTime = true;
+		whereChangeSum++;
+	}
+	}else
+	{
+			
+	}
+}
